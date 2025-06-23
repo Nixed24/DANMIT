38ea5430c4c60dc0b2f177050d416f7e9a47341d
Nicks24 <141518296+Nixed24@users.noreply.github.com>
2025-06-23 21:15:12 +0100

    Add files via upload

--- DANMIT.py
+++ DANMIT.py
@@ -12,13 +12,13 @@ with a uniform power (POWER, equal to 3 or 4) across all of them.
 
 REQUIRED LIBRARIES:
     numpy
-    Python Image Library (for generating the normal map image)
+    Python Image Library
 
-OTHER REQUIREMENTS:
-    Hammer++; specifically that the VMF be saved in Hammer++, since the "vertices_plus" field 
-    is used to obtain vertexes. This will be made optional in future.
+OPTIONAL:
+    Making your VMF using Hammer++, or converting it by copying it from Hammer/Hammer++ into a new file in Hammer++,
+    can speed up normal map creation for large maps.
     
-@author: NS
+@author: Nicks24 (GitHub)
 """
 
 import numpy as np
@@ -35,14 +35,17 @@ VMF_FILENAME = "normalmaptest6.vmf"
 POWER = 4
 #Power of all displacement maps in the VMF under VMF_FILENAME (MUST BE CONSTANT)
 
-Z_CEILING = 0
-#The top of all of the brushes in the VMF should be located at this Z coord.
+RES_X = 256
+#Horizontal resolution of normal map in pixels. Must be an integer power of 2.
 
-Z_FLOOR = -64
-#The bottom of all of the brushes in the VMF should be located at this Z coord.
+RES_Y = 256
+#Vertical resolution of normal map in pixels. Must be an integer power of 2.
 
 TILING_MODE = 0
 #The tiling mode for the normal map. 0 = untiled, 1 = tiled
+
+HAMMER_PLUS_PLUS = False
+#Is the VMF in Hammer++ format? Set to "False" if you're not sure.
 
 ################################################
 
@@ -126,13 +129,12 @@ def get_vertexes(vmf_data):
     """
     vertexes_array = np.zeros(3)
     while vmf_data.find('"v" "') != -1:
-        vmf_data = vmf_data[ vmf_data.find('"v" "') + 5 : ]
+        vmf_data = vmf_data[ vmf_data.find('"v" "') + 5 : ] 
+        #We want the index pointing to the major digit of the 1st number so we add 5 to the index to get there.
         _string = vmf_data[:vmf_data.find('"')]
         _string = np.array(_string.split(" "), dtype="float")
         vertexes_array = np.vstack((vertexes_array, _string))
     vertexes_array = vertexes_array[1:]
-    #This'll be dirty. Use str.find. We're going to use the brush centres to define brushes uniquely since
-    #brushes can share vertices and planes, but not centres!
     return vertexes_array
 def get_centre(vertexes_array):
     """Gets the average of all points specified when said points are a vertically-stacked ndarray of positions.
@@ -148,13 +150,106 @@ def get_centre(vertexes_array):
     centre_coord = np.zeros(3)
     for i in range (3):
         centre_coord[i] = np.average(vertexes_array[:,i])
-    #Take the average position of all vertices to get the center.
+    #Take the average position of all vertices to get the center. This almost definetly wouldn't work for planes.
     return centre_coord
+def get_fourth_vertex(plane_points):
+    """
+    Given a set of three vertices that correspond to a side that is perpendicular to 1 spatial (x/y/z) axis,
+    find the fourth vertex of that side.
+
+    Parameters
+    ----------
+    plane_points : np.ndarray
+        Vertically stacked ndarray of points corresponding to the three vertices given in the plane field of the side
+
+    Raises
+    ------
+    ValueError
+        When the 'perpendicular to 1 axis' condition is not met.
+
+    Returns
+    -------
+    fourth_point : np.ndarray
+        A numpy array corresponding (x/y/z wise) to the 4th vertex of the side.
+
+    """
+    #Assuming plane is a rectangle w.r.t x-y-z
+    #And the plane points are 3 vertices of that rectangle (they usually are in Hammer)
+    #We could generalise this to work in any axis but we'd have to map our plane to this situation,
+    #by choosing a different basis instead of x-y-z such that we have the ideal situation.
+    fourth_point = np.zeros(3)
+    const_axis = -1
+    x_s, y_s, z_s = plane_points[:,0], plane_points[:,1], plane_points[:,2]
+    point_array = np.array([x_s, y_s, z_s])
+    for c_i, c in enumerate(point_array):
+        if (np.average(c) - c[0]) == 0:
+            const_axis = c_i
+            const_axis_value = c[0]
+            ax_nt = np.delete(np.arange(3), c_i) #axes_nontrivial
+            points_of_interest = point_array
+            fourth_point[c_i] = const_axis_value
+    if const_axis == -1:
+        raise ValueError("No constant axis found in a side plane. Are all of your brushes cuboidal?")
+    #We know the plane that the points form will have normal vector in the direction of const_axis
+    
+    dupe_table = np.zeros(points_of_interest.shape)
+    for y in range(points_of_interest.shape[0]):
+        for x in range(points_of_interest.shape[1]):
+            # the value on a given axis with no dupes (i.e that value does not appear anywhere else on that axis)
+            # will be the value on that axis of our missing point, but the constant axis will of course be constant.
+            dupe_table[y][x] = np.sum(np.count_nonzero(points_of_interest[y] == points_of_interest[y][x]))
+    for a in ax_nt:
+        fourth_point[a] = point_array[a][dupe_table[a] == 1]
+    return fourth_point
+def get_plane_points(vmf_data):
+    """
+    Obtains the vertex positions from every plane field from specified VMF or VMF part as a vertically stacked ndarray.
+
+    Parameters
+    ----------
+    vmf_data : String
+        VMF data or part thereof
+
+    Returns
+    -------
+    vertexes_array : np.ndarray
+        Vertically stacked ndarray containing vertex positions as given by plane field.
+    """
+    planes_array = np.zeros((3,3))
+    while vmf_data.find('"plane" "') != -1:
+        vmf_data = vmf_data[ vmf_data.find('"plane" "') + 9 : ] 
+        #We want the index pointing to the major digit of the 1st number so we add 9 to the index to get there.
+        _string = vmf_data[:vmf_data.find('"')]
+        _string = _string.replace("(", "")
+        _string = _string.replace(")", "")
+        plane_array = _string.split(" ")
+        plane_array = np.array(plane_array, dtype="float")
+        plane_array = plane_array.reshape((3,3))
+        planes_array = np.vstack((planes_array, plane_array))
+    planes_array = planes_array[3:]
+    return planes_array
+def get_vertices_from_plane(plane_points):
+    """
+    Obtains the vertices of a side from its plane field.
+
+    Parameters
+    ----------
+    plane_points : np.ndarray
+        A vertically stacked ndarray of points given in the plane field of a side
+
+    Returns
+    -------
+    vertices : np.ndarray
+        A vertically stacked ndarray of points corresponding to the 4 vertices of that side.
+
+    """
+    fourth_point = get_fourth_vertex(plane_points)
+    vertices = np.vstack((plane_points, fourth_point))
+    return vertices
 
 vmf_data = read_vmf(VMF_FILENAME)
 
 NUM_OF_BRUSHES = vmf_data.count("solid\n\t{")
-
 
 #### Generating dictionary of { solid id : solid centre coord } ####
 solid_dict = {}
@@ -162,26 +257,53 @@ counter = 0
 solid_marker = vmf_data.find("solid\n\t{")
 trunc_vmf_data = vmf_data
 #solid\n\t{\n\t\t"id" "(num)"
-while solid_marker != -1:
-    
-    trunc_vmf_data = trunc_vmf_data[solid_marker:]
-    
-    end_marker = trunc_vmf_data[17:].find('"')
-    
-    solid_id = trunc_vmf_data[17:end_marker+17]
-    current_solid = get_field_by_id(vmf_data, "solid", solid_id, 1)
-    
-    vertexes_stack = get_vertexes(current_solid)
-    solid_centre = get_centre(vertexes_stack)
-    
-    solid_dict[solid_id] = solid_centre
-    
-    counter += 1
-    trunc_vmf_data = trunc_vmf_data[end_marker+17:]
-    solid_marker = trunc_vmf_data.find("solid\n\t{")
+if HAMMER_PLUS_PLUS == True:  
+    while solid_marker != -1:
+        
+        trunc_vmf_data = trunc_vmf_data[solid_marker:]
+        
+        end_marker = trunc_vmf_data[17:].find('"')
+        
+        solid_id = trunc_vmf_data[17:end_marker+17]
+        current_solid = get_field_by_id(vmf_data, "solid", solid_id, 1)
+         
+        vertexes_stack = get_vertexes(current_solid)
+
+        solid_centre = get_centre(vertexes_stack)
+        
+        solid_dict[solid_id] = solid_centre
+        
+        counter += 1
+        trunc_vmf_data = trunc_vmf_data[end_marker+17:]
+        solid_marker = trunc_vmf_data.find("solid\n\t{")
+else:
+    while solid_marker != -1:
+        
+        trunc_vmf_data = trunc_vmf_data[solid_marker:]
+        
+        end_marker = trunc_vmf_data[17:].find('"')
+        
+        solid_id = trunc_vmf_data[17:end_marker+17]
+        current_solid = get_field_by_id(vmf_data, "solid", solid_id, 1)
+        
+        vertexes_stack = np.zeros(3)
+        planes = get_plane_points(current_solid)
+        for i in range (6): #we expect our solid to be 6-sided
+            side_vertices = get_vertices_from_plane(planes[3*i : 3*(i+1)])
+            vertexes_stack = np.vstack((vertexes_stack, side_vertices))
+        vertexes_stack = vertexes_stack[1:]
+        
+        solid_centre = get_centre(vertexes_stack)
+        
+        solid_dict[solid_id] = solid_centre
+        
+        counter += 1
+        trunc_vmf_data = trunc_vmf_data[end_marker+17:]
+        solid_marker = trunc_vmf_data.find("solid\n\t{")
 ####################################################################
 
 #### Ordering the dictionary from left->right top->bottom from topleft w.r.t hammer top view ####
+
 #so least x, greatest y -> greatest x, least y; running through like a document
 #Going from least x to greatest x, decreasing y, then so on
 
@@ -208,7 +330,7 @@ x_centres = (x_length_solid_units * np.arange(0, (x_length_solids))) + X_MIN
 y_centres = (y_length_solid_units * (-1) * np.arange(0, (y_length_solids))) + Y_MAX
 
 
-nodes_per_solid = 2**POWER + 1
+nodes_per_solid = 2**POWER + 1 # Number of nodes along one direction for a displacement of given power.
 
 RESOLUTION_X = nodes_per_solid * x_length_solids
 RESOLUTION_Y = nodes_per_solid * y_length_solids
@@ -216,13 +338,15 @@ RESOLUTION_Y = nodes_per_solid * y_length_solids
         #### There is a bug in numpy where ndarrays change shape when you use them to assign a variable.
         #### This bug even occurs if you copy the array and then use the copy to assign a variable
         #### The original array, for some reason, changes in this case, to a different shape. This seems to be due to
-        #### the ndarray datatype being generic "object" and that object is an array-like, it will suddenly become int(0)
-
+        #### the ndarray datatype being generic "object" and that object is an ndarray, it will suddenly become int(0)
+        #### regardless of the content of the ndarray, I tried an ndarray of zeros and ndarray of a constant and the
+        #### same result was shown.
+        
 ####---Initialising arrays---####
-ordered_solid_id_list = np.zeros(NUM_OF_BRUSHES)
+ordered_solid_id_list = np.zeros(NUM_OF_BRUSHES) # ordered array of solid ids (order detailed further above)
 ordered_solid_centre_list = np.zeros(NUM_OF_BRUSHES, dtype=np.ndarray)
-ordered_solid_field_list = np.zeros(NUM_OF_BRUSHES, dtype=np.ndarray)
-ordered_solid_dispinfo_list = np.zeros(NUM_OF_BRUSHES, dtype=np.ndarray)
+ordered_solid_field_list = np.zeros(NUM_OF_BRUSHES, dtype=np.ndarray) # ordered array of "solid" fields from brush
+ordered_solid_dispinfo_list = np.zeros(NUM_OF_BRUSHES, dtype=np.ndarray) # ordered array of "dispinfo" fields from brush
 ordered_solid_distance_list = np.zeros(NUM_OF_BRUSHES, dtype=np.ndarray)
 ordered_solid_normals_list = np.zeros(NUM_OF_BRUSHES, dtype=np.ndarray)
 ordered_solid_direction_list = np.zeros(NUM_OF_BRUSHES, dtype=np.ndarray)
@@ -234,6 +358,7 @@ grand_displacement_field = np.zeros((nodes_per_solid * y_length_solids, nodes_pe
 ####--------------------------####
 
 
+####--- Declaring some functions that require variables that we declared earlier before initialising our arrays ---####
 def get_normals_sign(data):
     """
     Finds the sign field that is applied to the distance field to produce the displacement field.
@@ -401,10 +526,6 @@ def correct_displacement_field_orientation(displacement_field, config_num):
         return np.fliplr(displacement_field)
     else:
         return displacement_field
-    
-def get_vertices_from_plane():
-    #For compatability
-    return
 def gen_normal_map(displacement_field):
     """
     Generates a corresponding normal map from the distance field,
@@ -430,21 +551,18 @@ def gen_normal_map(displacement_field):
     shape = displacement_field.shape
     shape = shape + (3,) # This 2D array of single floats will be a 2D array of 3 values (RGB values)
     normal_map = np.zeros(shape, dtype=int)
-    theta_map = np.zeros(shape)
     
     for row in range(displacement_field.shape[0]):
         for index_x in range (len(displacement_field[0,:])-1):
             d = displacement_field[row][index_x] - displacement_field[row][index_x+1]
             theta_x = np.arcsin(d/np.sqrt(d**2+(node_distance_x)**2))
-            theta_map[row][index_x][0] = theta_x
-            normal_map[row][index_x][0] = np.floor((theta_x + (np.pi/2))*(255/np.pi))
+            normal_map[row][index_x][0] = np.floor(255 * ((theta_x / np.pi) + 0.5))
             
     for column in range(displacement_field.shape[1]):
         for index_y in range (len(displacement_field[:,0])-1):
             d = displacement_field[index_y][column] - displacement_field[index_y+1][column]
             theta_y = np.arcsin(d/np.sqrt(d**2+(node_distance_y)**2))
-            theta_map[index_y][column][1] = theta_y
-            normal_map[index_y][column][1] = np.floor((theta_y + (np.pi/2))*(255/np.pi))
+            normal_map[index_y][column][1] = np.floor(255 * ((theta_y / np.pi) + 0.5))
             
     normal_map[:,:,2] = displacement_field
     return normal_map
@@ -467,7 +585,7 @@ def normalise_distance_field(distance_field):
     min_dist = np.min(distance_field)
     if max_dist == min_dist:
         print("Cannot normalise distance field since max_dist = min_dist\n\
-              Aborted normalisation.")
+              Aborted normalisation, blue channel will be uniform")
         return distance_field
     normalised_distance_field = np.floor(distance_field * 128/(max_dist - min_dist))
     normalised_distance_field = normalised_distance_field + 128
@@ -497,14 +615,20 @@ def gen_normal_map_image(normal_map):
     """
     img = Image.new("RGB", (RESOLUTION_Y, RESOLUTION_X), (128, 128, 255))
     pixels = img.load()
-    for i in range(img.size[1]):    # for every col:
+    for i in range(img.size[1]):    # For every column
         for j in range(img.size[0]):    # For every row
             pixels[j,i] = (normal_map[i][j][0],\
                            normal_map[i][j][1],\
                            normal_map[i][j][2]) # set the colour accordingly
+    img = img.resize((RES_X, RES_Y))
     img.save(NMAP_FILENAME_NOEXT + (".png") + "")
     return img
 
+###############################################################################
+
+
+# #- Getting distance fields, combining with normals field to produce displacement field, combining displacement fields
+# into one grand displacement field -#
 counter = 0
 for y_index, current_y in enumerate(y_centres):
     
@@ -519,11 +643,14 @@ for y_index, current_y in enumerate(y_centres):
         ordered_solid_centre_list[counter] = target_centre
         ordered_solid_field_list[counter] = get_field_by_id(vmf_data, "solid", target_id[0][0], 1)
         
-        ##-- Generating ordered dispinfo dict -> displacement field dict (well, 2 lists, but essentially a dict) --##
+        ##-- Generating ordered dispinfo dict, hence generating displacement field dict --##
+        
+        #-- (well, 2 lists, but logically a dict) --##
+        
         ordered_solid_dispinfo_list[counter] = get_bracket_sandwich(ordered_solid_field_list[counter], "dispinfo", 3)
-        ordered_startpos_config_list[counter] = get_startpos_configuration(ordered_solid_dispinfo_list[counter], ordered_solid_centre_list[counter])
         ordered_solid_distance_list[counter] = get_bracket_sandwich(ordered_solid_dispinfo_list[counter], "distances", 4)
         ordered_solid_normals_list[counter] = get_bracket_sandwich(ordered_solid_dispinfo_list[counter], "normals", 4)
+        ordered_startpos_config_list[counter] = get_startpos_configuration(ordered_solid_dispinfo_list[counter], ordered_solid_centre_list[counter])
         ordered_solid_direction_list[counter] = get_normals_sign(ordered_solid_normals_list[counter])
         ordered_solid_distance_field[counter] = get_distances(ordered_solid_distance_list[counter])
         ordered_solid_displacement_field[counter] = (ordered_solid_direction_list[counter] * ordered_solid_distance_field[counter])
@@ -534,13 +661,12 @@ for y_index, current_y in enumerate(y_centres):
         grand_displacement_field[y_index * nodes_per_solid : (y_index + 1) * nodes_per_solid, x_index * nodes_per_solid : (x_index + 1) * nodes_per_solid] = displacement_field[y_index][x_index]
         
         counter += 1
+###############################################################################
 
-correction = np.rot90(np.flipud(grand_displacement_field), k = 3)
-
-normal_map = gen_normal_map(correction)
+normal_map = gen_normal_map(grand_displacement_field)
 normal_map[:,:,2] = normalise_distance_field(normal_map[:,:,2])
 
-# FOR TILING
+# FOR TILING #
 
 if TILING_MODE == 1:
     normal_map[-1,:] = normal_map[0,:]
@@ -548,5 +674,7 @@ if TILING_MODE == 1:
 elif TILING_MODE == 0:
     normal_map[-1,:] = np.array([127,127,192])
     normal_map[:,-1] = np.array([127,127,192])
+
+#  #  #  #  #
 
 normal_map_image = gen_normal_map_image(normal_map)
